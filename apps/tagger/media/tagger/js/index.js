f3c81c38a88d1a7741f2da2b3d5ef12a52a0d4d9
Egil Moeller <egil.moller@freecode.no>
2011-10-23 20:01:39 +0200

    Some more search stuff and ability to hide tag dialog

--- apps/tagger/media/tagger/js/index.js
+++ apps/tagger/media/tagger/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
   function updateSearch() {
     var tags = [];
-    $(".search #tags .tag").each(function () { tags.push(this.tag); });
+    $(".search .tags .tag").each(function () { tags.push(this.tag); });
 
     $.ajax({
       url: "/tagger/search",
@@ -17,27 +17,33 @@ $(document).ready(function () {
   }
 
   function removeTag(tag) {
-    $(".search #tags .tag_" + escape(tag)).remove();
+    $(".search .tags .tag_" + escape(tag)).remove();
     updateSearch();
   }
 
   function addTag(tag) {
     if (tag != '') {
-      $(".search #tags").append("<span class='tag tag_" + escape(tag) + "'>" + tag + "<a href='javascript: 0;' class='remove'>X</a></span> ");
-      $(".search #tags .tag:last-child")[0].tag = tag;
-      console.log([$(".search #tags .tag:last-child")[0], $(".search #tags .tag:last-child")[0].tag]);
-      $(".search #tags :last-child .remove").bind("click", function () {
+      $(".search .tags").append("<span class='tag tag_" + escape(tag) + "'>" + tag + "<a href='javascript: 0;' class='remove'>X</a></span> ");
+      $(".search .tags .tag:last-child")[0].tag = tag;
+      console.log([$(".search .tags .tag:last-child")[0], $(".search .tags .tag:last-child")[0].tag]);
+      $(".search .tags :last-child .remove").bind("click", function () {
 	removeTag(tag);
       });
     }
     updateSearch();
   }
 
-  $(".search #new_tag").autocomplete({source: "/tagger/tags/json"});
-  $(".search #new_tag").keypress(function (event) {
+  $(".search .new_tag").autocomplete({source: "/tagger/tags/json"});
+  $(".search .new_tag").keypress(function (event) {
     if (event.keyCode == 13) { // Enter...
-      addTag($(".search #new_tag")[0].value);
-      $(".search #new_tag")[0].value = '';
+      addTag($(".search .new_tag")[0].value);
+      $(".search .new_tag")[0].value = '';
+    }
+  });
+
+  $(".url").keypress(function (event) {
+    if (event.keyCode == 13) { // Enter...
+      document.location = "/tagger/view/" + escape($(".url")[0].value);
     }
   });
 });

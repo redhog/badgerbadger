99fcbe8d3503566f02bed082b7beed6c238937c4
Egil Moeller <egil.moller@freecode.no>
2011-10-24 17:34:22 +0200

    Merged in selectorgadget code into this repo to be able to hack stuff specifically for badgerbadger

---
+++ apps/tagger/media/tagger/js/selectorgadget/README.markdown
@@ -0,0 +1,3 @@
+[SelectorGadget](http://www.selectorgadget.com) is an open source bookmarklet that makes CSS selector generation and discovery on complicated sites a breeze.
+
+Please visit [http://www.selectorgadget.com](http://www.selectorgadget.com) to try it out.
---
+++ apps/tagger/media/tagger/js/selectorgadget/lib/selectorgadget_edge.js
@@ -0,0 +1,51 @@
+// Copyright (c) 2008, 2009 Andrew Cantino
+// Copyright (c) 2008, 2009 Kyle Maxwell
+
+function importJS(src, look_for, onload) {
+  var s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', src);
+  if (onload) wait_for_script_load(look_for, onload);
+  var head = document.getElementsByTagName('head')[0];
+  if (head) {
+    head.appendChild(s);
+  } else {
+    document.body.appendChild(s);
+  }
+}
+
+function importCSS(href, look_for, onload) {
+  var s = document.createElement('link');
+  s.setAttribute('rel', 'stylesheet');
+  s.setAttribute('type', 'text/css');
+  s.setAttribute('media', 'screen');
+  s.setAttribute('href', href);
+  if (onload) wait_for_script_load(look_for, onload);
+  var head = document.getElementsByTagName('head')[0];
+  if (head) {
+    head.appendChild(s);
+  } else {
+    document.body.appendChild(s);
+  }
+}
+
+function wait_for_script_load(look_for, callback) {
+  var interval = setInterval(function() {
+    if (eval("typeof " + look_for) != 'undefined') {
+      clearInterval(interval);
+      callback();
+    }
+  }, 50);
+}
+
+(function(){
+  importCSS('http://www.selectorgadget.com/unstable/lib/selectorgadget.css');
+  importJS('http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js', 'jQuery', function() { // Load everything else when it is done.
+    jQuery.noConflict();
+    importJS('http://www.selectorgadget.com/unstable/vendor/diff/diff_match_patch.js', 'diff_match_patch', function() {
+      importJS('http://www.selectorgadget.com/unstable/lib/dom.js', 'DomPredictionHelper', function() {
+        importJS('http://www.selectorgadget.com/unstable/lib/interface.js');
+      });
+    });
+  });
+})();
